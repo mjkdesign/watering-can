a1cd7858ceb520b459d30a2d3bc318a74f7d4cd7
Aaron Nealy <50381328+aaronnealy@users.noreply.github.com>
2019-08-09 11:06:27 -0500

    "staged changes"

---
+++ watering-can/server/server.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const path = require("path");
+const app = express();
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+app.use(cors());
+
+app.use(logger("dev"));
+
+const db = require("./models");
+
+const plantSeed = require("./scripts/seedDB");
+
+mongoose 
+    .connect(process.env.MONGODB_URI || "mongodb://localhost/Houseplants")
+
+    db.Plant.create(plantSeed)
+  .then(function(dbPlant) {
+    console.log(dbPlant);
+  })
+  .catch(function(err) {
+    console.log(err.message);
+  });
+
+
+    
+
+    const port = process.env.PORT || 3001;
+
+    app.listen(port, () => console.log(`Server starting on port ${port}`));
+
