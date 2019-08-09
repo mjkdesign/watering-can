613c14f959d715513738adc09e0460719d9b9cf4
hausu <kennedymj2@gmail.com>
2019-08-09 14:29:55 -0500

    set up passport

--- watering-can/server/server.js
+++ watering-can/server/server.js
@@ -6,34 +6,62 @@ const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const dbConnection = require('./database')
-const user = require('./routes/user')
+const dbConnection = require('./database');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
+const passport = require('./passport');
 
+const PORT = process.env.PORT || 3001;
+
+// ROUTE REQUIRES
+const user = require('./routes/user');
+
+// MIDDLEWARE
+app.use(logger('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+
+// SESSIONS
+
+app.use(
+	session({
+		secret: 'plant-pants', //pick a random string to make the hash that is generated secure
+		store: new MongoStore({ mongooseConnection: dbConnection }),
+		resave: false, //required
+		saveUninitialized: false //required
+	})
+)
+
+
+// PASSPORT
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
+
+// ROUTES
 app.use('/user', user)
 
-app.use(cors());
 
-app.use(logger("dev"));
+// const db = require("./models");
+// const plantSeed = require("./scripts/seedDB");
 
-const db = require("./models");
+// mongoose 
+//     .connect(process.env.MONGODB_URI || "mongodb://localhost/Houseplants")
 
-const plantSeed = require("./scripts/seedDB");
-
-mongoose 
-    .connect(process.env.MONGODB_URI || "mongodb://localhost/Houseplants")
-
-    db.Plant.create(plantSeed)
-  .then(function(dbPlant) {
-    console.log(dbPlant);
-  })
-  .catch(function(err) {
-    console.log(err.message);
-  });
+//     db.Plant.create(plantSeed)
+//   .then(function(dbPlant) {
+//     console.log(dbPlant);
+//   })
+//   .catch(function(err) {
+//     console.log(err.message);
+// });
 
 
-    
-
-    const port = process.env.PORT || 3001;
-
-    app.listen(port, () => console.log(`Server starting on port ${port}`));
+// START SERVER
+app.listen(PORT, () => {
+    console.log(`Server starting on http://localhost/${PORT}`)
+});
 
